b6f73ef7201b4355cb85974e3d547dbd2f276877
DJBotan <djbotan@mail.ru>
2023-11-09 14:10:16 +0000

    Обнаружена проблема с численными значениями в данных, найден способ решения

--- Main_script.R
+++ Main_script.R
@@ -5,3 +5,10 @@ id <- "1EggtVIwEYhWKgSUyKMTZOL3vVktEarl8" # google file ID
 data <- read_delim(sprintf("https://drive.google.com/u/0/uc?id=1EggtVIwEYhWKgSUyKMTZOL3vVktEarl8&export=download", id), delim = ";")
 
 view(data)
+
+data %>% 
+pull(cd4_cd8_ratio) %>% 
+  gsub(",", ".", .) %>% 
+  as.numeric()
+
+data
