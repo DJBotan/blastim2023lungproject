b057daf606dce0966be6be38083b54cec7371db8
DJBotan <djbotan@mail.ru>
2023-11-09 10:48:26 +0300

    test

--- Main_script.R
+++ Main_script.R
@@ -3,3 +3,5 @@ library(tidyverse)
 
 id <- "1EggtVIwEYhWKgSUyKMTZOL3vVktEarl8" # google file ID
 data <- read_delim(sprintf("https://drive.google.com/u/0/uc?id=1EggtVIwEYhWKgSUyKMTZOL3vVktEarl8&export=download", id), delim = ";")
+
+view(data)
